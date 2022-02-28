c723b76e30ec719d34521bdcab96d388ea262848
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2022-02-28 13:17:15 -0600

    Create PushConfig.sh

---
+++ PushConfig.sh
@@ -0,0 +1,7 @@
+#To get my settings in when borrowing a computer
+git config user.name "NathanNuckels"
+git config user.email "nathannuckels@gmail.com"
+git config core.editor "nano -l "
+git config pull.ff false
+git config merge.ff false
+echo "Stuff done."
