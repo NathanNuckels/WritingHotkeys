667a595788f399f19fd4f6311f0dc5926391bf0e
Nathan Nuckels <nathannuckels@gmail.com>
2022-03-02 20:05:06 -0600

    Fixed levelup.py args
    Looks like I just don't understand >= and <=

--- levelup.py
+++ levelup.py
@@ -6,39 +6,37 @@ except ModuleNotFoundError:
     print("To install it, type")
     print("\tpip install keyboard")
     exit(1)
+
+def raiseError(_input):
+    print("\nAn error has occured. View the last line of the Traceback for info.")
+    print("\n")
+    raise Exception(_input)
+
 delete=False
 messages=0
 delay=1
-if len(sys.argv)>1:
-    if sys.argv[1].lower=="-help" or sys.argv[2].lower=="--help":
-        print("Syntax: python levelup.py [bool: DELETE] [int: MESSAGES] [float: DELAY]")
-        print("\tDelete: delete messages?")
-        print("\tMessages: Number if messages. Set to 0 for infinite.")
-        print("\tDelay: The delay in seconds between messages. Must be greater than 1")
-        exit(0)
-    if sys.argv[1].lower=="true":
+args=sys.argv[1:]
+if len(args)>0:
+    #Get delete varible
+    if args[0].lower()=="true":
         delete=True
-    elif sys.argv[1].lower=="false":
+    elif args[0].lower()=="false":
         delete=False
     else:
-        print("Error: DELETE must be true or false.")
-        exit(1)
-    if len(sys.argv)>2:
-        try:
-            messages=int(sys.argv[2])
-        except ValueError:
-            print("Error: MESSAGES must be a number")
-            exit(1)
-        if messages<=0:
-            print("Error: MESSAGES must not be negitive.")
-            exit(1)
-        if len(sys.argv)>3:
-            try:
-                delay=int(sys.argv[3])
-            except ValueError:
-                print("Error: DELAY must be a number")
-                exit(1)
-            if delay<1:
-                print("Error: DELAY must be greater than 1")
-                exit(1)
+        raiseError("Varible DELETE must be a bool.")
+if len(args)>1:
+    try:
+        messages=int(args[1])
+    except ValueError:
+        raiseError("Varible MESSAGES must be an int.")
+if len(args)>2:
+    try:
+        delay=int(args[2])
+    except ValueError:
+        raiseError("Varible DELAY must be an int.")
+    if delay<1:
+        raiseError("Varible DELAY must be greater than or equal to 1")
+if len(args)>3:
+    raiseError("Too many arguments.")
+
 print([delete,messages,delay])
